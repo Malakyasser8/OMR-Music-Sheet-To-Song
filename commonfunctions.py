21e3d09b5cb525c81a59a3a8e4fd6435fbdcb4ef
Tahoun99 <amkmtahoon@gmail.com>
2024-12-07 21:29:00 +0200

    detect the position of note heads

--- commonfunctions.py
+++ commonfunctions.py
@@ -23,23 +23,42 @@ from skimage.color import label2rgb
 from skimage.filters import sobel_h, sobel, sobel_v,roberts, prewitt
 
 # Show the figures / plots inside the notebook
-def show_images(images,titles=None):
-    #This function is used to show image(s) with titles by sending an array of images and an array of associated titles.
-    # images[0] will be drawn with the title titles[0] if exists
-    # You aren't required to understand this function, use it as-is.
+# def show_images(images,titles=None):
+#     #This function is used to show image(s) with titles by sending an array of images and an array of associated titles.
+#     # images[0] will be drawn with the title titles[0] if exists
+#     # You aren't required to understand this function, use it as-is.
+#     n_ims = len(images)
+#     if titles is None: titles = ['(%d)' % i for i in range(1,n_ims + 1)]
+#     fig = plt.figure()
+#     n = 1
+#     for image,title in zip(images,titles):
+#         a = fig.add_subplot(1,n_ims,n)
+#         if image.ndim == 2: 
+#             plt.gray()
+#         plt.imshow(image)
+
+#         a.set_title(title)
+#         n += 1
+#     fig.set_size_inches(np.array(fig.get_size_inches()) * n_ims)
+#     plt.show() 
+def show_images(images, titles=None):
     n_ims = len(images)
-    if titles is None: titles = ['(%d)' % i for i in range(1,n_ims + 1)]
+    if titles is None: 
+        titles = ['(%d)' % i for i in range(1, n_ims + 1)]
     fig = plt.figure()
     n = 1
-    for image,title in zip(images,titles):
-        a = fig.add_subplot(1,n_ims,n)
+    for image, title in zip(images, titles):
+        a = fig.add_subplot(1, n_ims, n)
         if image.ndim == 2: 
             plt.gray()
         plt.imshow(image)
+        plt.colorbar()  # Add color bar to show numbers
         a.set_title(title)
+        plt.xticks(np.arange(0, image.shape[1], step=10))  # Set x-axis ticks
+        plt.yticks(np.arange(0, image.shape[0], step=10))  # Set y-axis ticks
         n += 1
     fig.set_size_inches(np.array(fig.get_size_inches()) * n_ims)
-    plt.show() 
+    plt.show()
 
 
 def showHist(img):
